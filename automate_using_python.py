a68e964443c71fe40dc615ee8da88f05cb15e027
IHEANYI, FAVOUR CHISOM <iheanyifavour.ube3room312@gmail.com>
2022-08-19 17:36:57 +0100

    Added automate_using_python.py
    description

---
+++ automate_using_python.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+website = "https://www.thesun.co.uk/sport/football/"
+path = "C:\Users\CHISOM\Downloads/chromedriver_win32"
+
+service = Service(executable_path=path)
+driver = webdriver.Chrome(service=service)
+driver.get(website)
