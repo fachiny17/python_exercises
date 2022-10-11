fc09ce2eece7cfb74a01f2557b7b2ad71e9d4b85
IHEANYI, FAVOUR CHISOM <iheanyifavour.ube3room312@gmail.com>
2022-10-11 22:56:10 +0100

    Made an update

--- automate_using_python.py
+++ automate_using_python.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 website = "https://www.thesun.co.uk/sport/football/"
 path = "C:\Users\CHISOM\Downloads/chromedriver_win32"
 
+
 service = Service(executable_path=path)
 driver = webdriver.Chrome(service=service)
 driver.get(website)
