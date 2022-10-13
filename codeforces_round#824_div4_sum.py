86f501e737b37fb323a908052cb37b996d75177c
IHEANYI, FAVOUR CHISOM <iheanyifavour.ube3room312@gmail.com>
2022-10-13 18:17:00 +0100

    added Codeforces Round #824 Div 4 exercise title = Sum

---
+++ codeforces_round#824_div4_sum.py
@@ -0,0 +1,19 @@
+lst = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,5,16,17,18,19,20]
+
+a = int(input('a = '))
+b = int(input('b = '))
+c  = int(input('c = '))
+
+num = (a) + (b)
+pep = (a) + (c)
+og = (b) + (c)
+
+
+if int(c) == num and c in lst:
+    print('YES')
+elif int(a) == og and a in lst:
+    print('YES')
+elif int(b) == pep and b in lst:
+    print('YES')
+else:
+    print('NO')
